541df046468d478bbb08ef915f6641ace44aff6d
himanshu-wq <54060784+himanshu-wq@users.noreply.github.com>
2020-06-09 23:01:06 +0530

    calculator now works with one number

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -1,8 +1,15 @@
 package is.ru.stringcalculator;
-
 public class Calculator {
 
 	public static int add(String text){
+		if(text.equals("")){
 			return 0;
+		}
+		else
+			return toInt(text);
 	}
-}
+
+	private static int toInt(String number){
+		return Integer.parseInt(number);
+	}
+} 
