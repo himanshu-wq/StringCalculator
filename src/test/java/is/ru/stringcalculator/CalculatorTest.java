82b189f6ed271efb5b01f4ffc81909f83723d4d7
himanshu-wq <54060784+himanshu-wq@users.noreply.github.com>
2020-06-11 21:02:08 +0530

    works with , or newline as a divider

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -17,9 +17,13 @@ public class CalculatorTest {
 	public void testTwoNumbers(){
 		assertEquals(3, Calculator.add("1,2"));
 	}
-
 	@Test
     public void testThreeNumbers(){
     	assertEquals(6, Calculator.add("1,2,3"));
+    }
+
+    @Test
+    public void testNewLine(){
+    	assertEquals(6, Calculator.add("1\n2,3"));
     }
 } 
