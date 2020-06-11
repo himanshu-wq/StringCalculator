81aedf18d51b2160c830b6ee7e55d1bef2c10122
himanshu-wq <54060784+himanshu-wq@users.noreply.github.com>
2020-06-11 21:06:17 +0530

    Negative numbers throw exception

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -5,8 +5,6 @@ public class Calculator {
 			return 0;
 		}
 		else{
-			text.replaceAll("\n", ",")
-			String numList[] = splitNumbers(text, ",");
 			String numList[] = splitNumbers(text, ",|\n");
 			return sum(numList);
 		}
@@ -17,11 +15,25 @@ public class Calculator {
 	private static String[] splitNumbers(String numbers, String divider){
 	    return numbers.split(divider);
 	}
+
 	private static int sum(String[] numbers){
  	    int total = 0;
+ 	    String negString = "";
+
         for(String number : numbers){
+        	if(toInt(number) < 0){
+        		if(negString.equals(""))
+        			negString = number;
+        		else
+        			negString += ("," + number);
+        	}
 		    total += toInt(number);
 		}
+
+		if(!negString.equals("")){
+			throw new IllegalArgumentException("Negatives not allowed: " + negString);
+		}
+
 		return total;
     }
 }
