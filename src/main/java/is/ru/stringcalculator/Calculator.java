75193f9cd319956ead5aa9c9d69a56b0ac0da003
himanshu-wq <54060784+himanshu-wq@users.noreply.github.com>
2020-06-11 09:29:02 +0530

    calculator now works with two numbers and , as a divider

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -1,15 +1,30 @@
 package is.ru.stringcalculator;
 public class Calculator {
-
 	public static int add(String text){
 		if(text.equals("")){
 			return 0;
 		}
 		else
 			return toInt(text);
+		else{
+			String numList[] = splitNumbers(text, ",");
+			return sum(numList);
+		}
 	}
 
 	private static int toInt(String number){
 		return Integer.parseInt(number);
 	}
-} 
+
+	private static String[] splitNumbers(String numbers, String divider){
+	    return numbers.split(divider);
+	}
+
+	private static int sum(String[] numbers){
+ 	    int total = 0;
+        for(String number : numbers){
+		    total += toInt(number);
+		}
+		return total;
+    }
+}
